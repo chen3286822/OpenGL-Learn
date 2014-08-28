a8a71e3a705f397317e57e6a727d229c96de3fd6
chenfeifei <chen3286822@yeah.net>
2014-08-28 22:26:37 +0800

    learn 12(阴影继续)

--- OpenGL/Shadow.cpp
+++ OpenGL/Shadow.cpp
@@ -3,13 +3,44 @@
 
 void ShadowTest::init(void)
 {
+	glewInit();
+	glClearColor(0.0, 0.0, 0.0, 0.0);/* select clearing color   */
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_DEPTH_TEST);
+	glFrontFace(GL_CCW);
+	glEnable(GL_CULL_FACE);
+}
 
+void ShadowTest::reshape(GLint w, GLint h)
+{
+	//保持绘制场景不会变形，且始终位于窗口的中央
+	GLint newW = w > h ? h : w;
+	glViewport((w - newW) / 2, (h - newW) / 2, (GLsizei)newW, (GLsizei)newW);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20);
+	gluPerspective(60.0, 1.0, 1.5, 350);
+	//glOrtho(-10.0, 10.0, -10.0, 10.0, 1.5, 20);
+	
 }
 
 void ShadowTest::display(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);/* clear all pixels   */
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);/* clear all pixels   */
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 50.0, 300.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
+	//地面
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_QUADS);
+	glVertex3f(-100, -100, -100);
+	glVertex3f(-100, -100, 100);
+	glVertex3f(100, -100, 100);
+	glVertex3f(100, -100, -100);
+	glEnd();
+
+	glutSolidSphere(50, 50, 50);
 
 	glFlush();
 }
--- OpenGL/Shadow.h
+++ OpenGL/Shadow.h
@@ -8,6 +8,7 @@ public:
 	CreateTest(ShadowTest);
 	virtual void init(void);
 	virtual void display(void);
+	virtual void reshape(GLint w, GLint h);
 private:
 	//创建一个阴影投影矩阵，根据平面方程的系数以及光照的位置创建的
 	//存储在mat中
